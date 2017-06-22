66d842938712bf04de0b4552cdf30629151a6811
Sarah Marsh <Sarah.Marsh@arm.com>
2017-06-22 16:01:08 -0500

    Write log messages to a text file on SD card

--- main.cpp
+++ main.cpp
@@ -13,18 +13,29 @@ int num;
 char dirname[16];
 DigitalOut led2(LED2);
 
-void return_error(int ret_val) {
-  if (ret_val)
-    printf("Failure. %d\r\n", ret_val);
-  else
-    printf("done.\r\n");
+void app_log(const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    FILE* fd = fopen("/fs/log.txt", "a");
+    vfprintf(fd, fmt, args);
+    fclose(fd);
+    va_end(args);
 }
 
 void errno_error(void* ret_val) {
   if (ret_val == NULL)
-    printf(" Failure. %d \r\n", errno);
+    app_log(" Failure. %d \r\n", errno);
   else
-    printf(" done.\r\n");
+    app_log(" done.\r\n");
+}
+
+
+void return_error(int ret_val) {
+  if (ret_val)
+    app_log("Failure. %d\r\n", ret_val);
+  else
+    app_log("done.\r\n");
 }
 
 int format_fs(){
@@ -32,14 +43,20 @@ int format_fs(){
     printf("Welcome to the filesystem example.\r\n"
            "Formatting a FAT, RAM-backed filesystem. ");
     error = FATFileSystem::format(&bd);
-    return_error(error);
+    if (error)
+      printf("Failure. %d\r\n", error);
+    else
+      printf("done.\r\n");
     return error;
 }
 
 int mount_fs() {
     printf("Mounting the filesystem on \"/fs\". ");
     int error = fs.mount(&bd);
-    return_error(error);
+    if (error)
+      printf("Failure. %d\r\n", error);
+    else
+      printf("done.\r\n");
     return error;
 }
 
@@ -51,25 +68,25 @@ void init_fs() {
 }
 
 void take_and_store_photo() {
-    printf("Taking photo\r\n");
+    app_log("Taking photo\r\n");
     if(camera.take_picture() == 0) {
-        printf("Took photo\r\n");
+        app_log("Took photo\r\n");
         uint32_t size = camera.get_picture_size();
         uint8_t* data_buff = (uint8_t*)malloc(size);
         uint32_t bytes_read = camera.read_picture_data(data_buff, size);
 
         char filename[30];
         sprintf(filename, "/fs/%s/img%d.jpg", dirname, num++);
-        printf("Opening a new file, %s\r\n", filename);
+        app_log("Opening a new file, %s\r\n", filename);
         FILE* fd = fopen(filename, "wb");
         errno_error(fd);
         fwrite(data_buff, sizeof(uint8_t), bytes_read, fd);
         fclose(fd);
-        printf("file written\r\n");
+        app_log("file written\r\n");
         free(data_buff);
     }
     else {
-        printf("Failed to take photo\r\n");
+        app_log("Failed to take photo\r\n");
     }
 }
 
@@ -78,11 +95,11 @@ void create_dir() {
     int dir_num = 0;
     char temp_dir[7];
     sprintf(temp_dir, "dir%d", dir_num);
-    printf("Trying to open %s\r\n", temp_dir);
+    app_log("Trying to open %s\r\n", temp_dir);
     DIR* dir = fs.opendir(temp_dir);
     return_error(errno);
     if (dir == NULL) {
-        printf("Creating %s\r\n", temp_dir);
+        app_log("Creating %s\r\n", temp_dir);
         fs.mkdir(temp_dir, O_RDWR);
         return_error(errno);
         strcpy(dirname, temp_dir);
@@ -91,13 +108,13 @@ void create_dir() {
         while(1) {
             memset(temp_dir, 0, sizeof temp_dir);
             sprintf(temp_dir, "dir%d", ++dir_num);
-            printf("Trying to open %s\r\n", temp_dir);
+            app_log("Trying to open %s\r\n", temp_dir);
             DIR* dir = fs.opendir(temp_dir);
             if (dir == NULL) {
                 break;
             }
         }
-        printf("Creating %s\r\n", temp_dir);
+        app_log("Creating %s\r\n", temp_dir);
         int err  = fs.mkdir(temp_dir, O_RDWR);
         return_error(err);
         strcpy(dirname, temp_dir);
@@ -109,15 +126,14 @@ void blink() {
 }
 
 int main() {
-    camera.powerup();
     init_fs();
+    camera.attach_debug_function(&app_log);
+    camera.powerup();
     create_dir();
     num = 0;
     take_and_store_photo();
     queue.call_every(600000, take_and_store_photo);
     queue.call_every(500, blink);
     queue.dispatch();
-    while(1) {
-        wait(0.2);
-    }
+
 }
